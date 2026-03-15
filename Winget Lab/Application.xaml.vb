561e97334388a589d451d0fcb3740644551b060f
Bruce Murphy <brucemu@microsoft.com>
2026-03-15 10:31:14 -0700

    WingetLab v2.1 03152026

--- Winget Lab/Application.xaml.vb
+++ Winget Lab/Application.xaml.vb
@@ -1,6 +1,25 @@
-﻿Class Application
+﻿Imports System.Threading.Tasks
 
-    ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
-    ' can be handled in this file.
+Partial Public Class Application
+    Inherits System.Windows.Application
+
+    Protected Overrides Async Sub OnStartup(e As StartupEventArgs)
+        MyBase.OnStartup(e)
+
+        ShutdownMode = ShutdownMode.OnExplicitShutdown
+
+        Dim splash = New SplashWindow()
+        splash.Show()
+
+        Await Task.Delay(TimeSpan.FromSeconds(1.5))
+
+        splash.Close()
+
+        Dim mainWindow = New MainWindow()
+        mainWindow = mainWindow
+        mainWindow.Show()
+
+        ShutdownMode = ShutdownMode.OnMainWindowClose
+    End Sub
 
 End Class
---
+++ Winget Lab/SplashWindow.xaml.vb
@@ -0,0 +1,8 @@
+Partial Public Class SplashWindow
+    Inherits System.Windows.Window
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+End Class
